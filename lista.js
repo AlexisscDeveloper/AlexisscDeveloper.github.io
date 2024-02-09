9ecac2a7094bbfbbcbe88cf1339824da02ee1ac7
AlexisscDeveloper <156980431+AlexisscDeveloper@users.noreply.github.com>
2024-02-08 22:11:45 -0600

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let PersonajeID = 0; // Empezamos con el ID 0 que no existe
+    let PersonajeID = 0; //Empezamos con el ID 0 que no existe
 
     document.querySelector(".btn-primary").addEventListener("click", async (event) => {
         var itemlist = document.getElementById("my-list");
@@ -7,28 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
         var total = itemlist.childElementCount + 1;
         var clone = document.importNode(template.content, true);
 
-        PersonajeID++;
+        PersonajeID++; //hacer que vaya incrementando el ID cada vez que se le de al botón de "agregar personaje" 
 
         const response = await fetch(`https://rickandmortyapi.com/api/character/${PersonajeID}`);
         const data = await response.json();
 
         clone.querySelector("[data-id='number']").textContent = `${total}`;
+
+        // Se crea una variable para poder almacenar la imagen del personaje
         clone.querySelector("[data-id='imagen']").src = data.image;
-        clone.querySelector("[data-id='Id']").textContent = `${data.id}`;
-        clone.querySelector("[data-id='Nombre']").textContent = `${data.name}`;
-        clone.querySelector("[data-id='Estatus']").textContent = `${data.status}`;
-        clone.querySelector("[data-id='Especie']").textContent = `${data.species}`;
+        clone.querySelector("[data-id='Id']").textContent =  `${data.id}`;
+        clone.querySelector("[data-id='Nombre']").textContent =  `${data.name}`;
+        clone.querySelector("[data-id='Estatus']").textContent =  `${data.status}`;
+        clone.querySelector("[data-id='Especie']").textContent =  `${data.species}`;
 
         // Aplicar estilos flex al contenedor de la lista para que los elementos se alineen horizontalmente
-        clone.style.flex = "0 0 auto"; // Establecer el tamaño automático
         itemlist.style.display = "flex";
-        itemlist.style.flexWrap = "wrap"; // Permitir que los elementos se envuelvan a la siguiente fila
         itemlist.appendChild(clone);
     });
 
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemlist = document.getElementById("my-list");
         itemlist.replaceChildren();
-        PersonajeID = 0; // Reinicia el PersonajeID a cero al darle al botón de limpiar
+        PersonajeID = 0; /*Hace que al darle al botón de limpiar se reinicie el PersonajeID a cero y empiece de nuevo con el 1*/
     });
 });
