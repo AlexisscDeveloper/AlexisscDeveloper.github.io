54a99e6dec70bd247b03fe46caf182b6fc2b0a46
AlexisscDeveloper <156980431+AlexisscDeveloper@users.noreply.github.com>
2024-02-08 21:38:03 -0600

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,44 +1,34 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    let PersonajeID = 0; // Empezamos con el ID 0 que no existe
+let PersonajeID = 0; //Empezamos con el ID 0 que no existe
     
-    document.querySelector(".btn-primary").addEventListener("click", async (event) => {
+    document.querySelector(".btn-primary").addEventListener("click",async (event) => {
         var itemlist = document.getElementById("my-list");
         var template = document.getElementById("list-template");
         var total = itemlist.childElementCount + 1;
         var clone = document.importNode(template.content, true);
-
-        PersonajeID++; // Hacer que vaya incrementando el ID cada vez que se le de al botón de "agregar personaje" 
         
-        const response = await fetch(`https://rickandmortyapi.com/api/character/${PersonajeID}`);
-        const data = await response.json();
+
+        
+        PersonajeID++; //hacer que vaya incrementando el ID cada vez que se le de al boton de "agregar personaje" 
+        
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${PersonajeID}`); //Me traigo la API asi mismo agregamos la variable para que el ID vaya incrementando
+        const data = await response.json(); //Lo convertimos a JSON, ya que si no se convierte los datos me los pone indefinidos
 
         clone.querySelector("[data-id='number']").textContent = `${total}`;
         
-        // Se crea una variable para poder almacenar la imagen del personaje
+        //Se crea una variable para poder almacenar la imagen del personaje
         clone.querySelector("[data-id='imagen']").src = data.image;
         clone.querySelector("[data-id='Id']").textContent =  `${data.id}`;
         clone.querySelector("[data-id='Nombre']").textContent =  `${data.name}`;
         clone.querySelector("[data-id='Estatus']").textContent =  `${data.status}`;
         clone.querySelector("[data-id='Especie']").textContent =  `${data.species}`;
-
-        // Cambiar el estilo para que los elementos se agreguen uno al lado del otro
-        clone.querySelector("[data-id='number']").style.display = "inline-block";
-        clone.querySelector("[data-id='imagen']").style.display = "inline-block";
-        clone.querySelector("[data-id='Id']").style.display = "inline-block";
-        clone.querySelector("[data-id='Nombre']").style.display = "inline-block";
-        clone.querySelector("[data-id='Estatus']").style.display = "inline-block";
-        clone.querySelector("[data-id='Especie']").style.display = "inline-block";
-        
         itemlist.appendChild(clone);
     });
 
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemlist = document.getElementById("my-list");
         itemlist.replaceChildren();
-        PersonajeID = 0; // Reinicia el PersonajeID a cero al darle al botón de limpiar
+       PersonajeID = 0; /*Hace que al darle al boton de limpiar se reinicie el PersonajeID a cero y empiece de nuevo con el 1*/
     });
 });
-
 
