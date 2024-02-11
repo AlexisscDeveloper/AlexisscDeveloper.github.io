99cf2e9d8c46cf216e52e8a5003a049682070b16
AlexisscDeveloper <156980431+AlexisscDeveloper@users.noreply.github.com>
2024-02-11 15:11:39 -0600

    Update lista2.js

--- lista2.js
+++ lista2.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let page = 1; // Página actual de la API
+    let pagina = 1; // Cuando se inicialice la pagina por default ponemos una variable que empiece con la pagina 1
     
     document.querySelector(".btn-primary").addEventListener("click", async (event) => {
         const itemlist = document.getElementById("my-list");
@@ -7,27 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
         const total = itemlist.childElementCount + 1;
         
         // Hacer la solicitud a la API con el ID del primer personaje en la página actual
-        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${pagina}`);
         const data = await response.json();
         
-        data.results.forEach(async character => {
+        data.results.forEach(async personaje => {
             const clone = document.importNode(template.content, true);
             clone.querySelector("[data-id='number']").textContent = `${total}`;
-            clone.querySelector("[data-id='imagen']").src = character.image;
-            clone.querySelector("[data-id='Id']").textContent = character.id;
-            clone.querySelector("[data-id='Nombre']").textContent = character.name;
-            clone.querySelector("[data-id='Estatus']").textContent = character.status;
-            clone.querySelector("[data-id='Especie']").textContent = character.species;
+            clone.querySelector("[data-id='imagen']").src = personaje.image;
+            clone.querySelector("[data-id='Id']").textContent = personaje.id;
+            clone.querySelector("[data-id='Nombre']").textContent = personaje.name;
+            clone.querySelector("[data-id='Estatus']").textContent = personaje.status;
+            clone.querySelector("[data-id='Especie']").textContent = personaje.species;
             itemlist.appendChild(clone);
         });
         
-        page++; // Incrementar la página para la próxima solicitud
+        pagina++; // Incrementamos la variable "pagina" para que cuando se le de de nuevo al boton, me ponga ahora la siguiente pagina que corresponda 
     });
 
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         const itemlist = document.getElementById("my-list");
         itemlist.replaceChildren();
-        page = 1; // Reiniciar la página al limpiar la lista
+        pagina = 1; // Reinciamos la variable "pagina" a su valor por default que es 1
     });
 });
 
