96c8b63bd8c5abce2276b9555410e276019a442a
AlexisscDeveloper <156980431+AlexisscDeveloper@users.noreply.github.com>
2024-02-11 15:27:30 -0600

    Update lista2.js

--- lista2.js
+++ lista2.js
@@ -19,9 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
             clone.querySelector("[data-id='Estatus']").textContent = personaje.status;
             clone.querySelector("[data-id='Especie']").textContent = personaje.species;
             itemlist.appendChild(clone);
+
+            pagina++;
         });
         
-        pagina++; // Incrementamos la variable "pagina" para que cuando se le de de nuevo al boton, me ponga ahora la siguiente pagina que corresponda 
+        //pagina++; // Incrementamos la variable "pagina" para que cuando se le de de nuevo al boton, me ponga ahora la siguiente pagina que corresponda 
     });
 
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
